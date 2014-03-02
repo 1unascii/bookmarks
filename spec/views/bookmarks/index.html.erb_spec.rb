a16571a4b1913a7e19eb76511d2683fb7b76d7a6
MATC Student <student@MATC1234.(none)>
2014-03-01 23:54:37 -0600

    updated to test for a link with the specified name, still can't figure out how to test for what's in the *href attribute

--- spec/views/bookmarks/index.html.erb_spec.rb
+++ spec/views/bookmarks/index.html.erb_spec.rb
@@ -17,7 +17,11 @@ describe "bookmarks/index" do
   it "renders a list of bookmarks" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Url".to_s, :count => 2
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
+    assert_select "tr>td>a", "Name".to_s 
+    #Really can't figure out how to test the href attribute value
+    #should have_xpath("//a[@href = " + "Url".to_s + "]")
+    #find('a')["href=" + "Url".to_s].should be_visible
+    #page.should have_link("Name".to_s, :href => "Url".to_s)
+    #assert_select "tr>td", :text => "Name".to_s, :count => 2//why is the count there? what is it counting? I know there are more than two of these.
   end
 end
